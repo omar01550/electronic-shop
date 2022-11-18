0411b2969944b757721811e83d7add4aaaa8fb0c
omar01550 <onmeldone@gmail.com>
2022-11-18 21:53:52 +0700

    add ts language

--- src/ts/index.ts
+++ src/ts/index.ts
@@ -1,34 +1,47 @@
-"use strict";
 //handel categories Sectrion
+
 let allCatigories = document.querySelectorAll(".image");
+
 allCatigories.forEach((ele, i) => {
-    ele.addEventListener("click", function () {
-        window.location = ele.dataset.target;
-    });
+    ele.addEventListener("click",function () {
+        window.location=ele.dataset.target;
+    })
 });
+
+
+
 let popularMobilesSection = document.querySelector(".popular-mobiles .container");
 let arrowLeft = document.querySelector(".popular-mobiles .arrow-left");
 let arrowRight = document.querySelector(".popular-mobiles .arrow-right");
 let mobilesCard = document.querySelectorAll(".mobile"); // all mobiles
+
+
 mobilesCard.forEach((mobile, i) => {
-    mobile.onclick = function () {
-        window.location = 'details.html';
-    };
+    mobile.onclick=function () {
+        window.location= 'details.html';
+    }
 });
-arrowRight.addEventListener("click", scrollToRight);
-arrowLeft.addEventListener("click", scrollToLeft);
-function scrollToRight() {
-    popularMobilesSection.scrollLeft += 350;
+
+arrowRight.addEventListener("click",scrollToRight);
+arrowLeft.addEventListener("click",scrollToLeft);
+
+function scrollToRight() :void{
+    popularMobilesSection.scrollLeft+=350;
 }
-function scrollToLeft() {
-    popularMobilesSection.scrollLeft -= 350;
+function scrollToLeft() :void{
+ popularMobilesSection.scrollLeft-=350;
 }
+
+
 let popularWatchesSection = document.querySelector(".popular-watches .container");
 let arrowLeftWatches = document.querySelector(".popular-watches .arrow-left");
 let arrowRighttWatches = document.querySelector(".popular-watches .arrow-right");
-arrowRighttWatches.addEventListener("click", function () {
-    popularWatchesSection.scrollLeft += 350;
+
+
+arrowRighttWatches.addEventListener("click",function () :void{
+   popularWatchesSection.scrollLeft+=350;
 });
-arrowLeftWatches.addEventListener("click", function () {
-    popularWatchesSection.scrollLeft -= 350;
+
+arrowLeftWatches.addEventListener("click",function () :void{
+   popularWatchesSection.scrollLeft-=350;
 });
